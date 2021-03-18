458cbc1230c366f3daf8c63fb28c330ed92bd8de
Helen-Noe <helenmyatnoe@gmail.com>
2021-03-18 22:00:19 +1100

    trying to implement search bar

--- PokedexSwiftUI/View/PokedexView.swift
+++ PokedexSwiftUI/View/PokedexView.swift
@@ -11,12 +11,22 @@ struct PokedexView: View {
 	
 	private let gridItems = [GridItem(.flexible()), GridItem(.flexible())]
 	@ObservedObject var viewModel = PokemonViewModel()
+	@State var searchTerm = ""
 	
     var body: some View {
 		NavigationView{
 			ScrollView{
+				HStack{
+					TextField("Search Term Here", text: $searchTerm)
+				}
+				.padding()
+				.background(Color(.systemGray5))
+				.cornerRadius(12)
+				.padding()
+
 				LazyVGrid(columns: gridItems, spacing: 16){
-					ForEach(viewModel.pokemon){ pokemon in
+					ForEach(viewModel.pokemon.filter{ searchTerm.isEmpty ? true : $0.name.contains(searchTerm)
+					}){ pokemon in
 						PokemonCell(pokemon: pokemon, viewModel: viewModel)
 					}
 				}
